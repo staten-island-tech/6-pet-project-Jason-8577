f83e23899fd7229a0df94be5b8383fb89dd44719
Jason-8577 <jasono119@nycstudents.net>
2025-11-17 06:26:03 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,28 @@
+""" class Hero:
+    def __init__(self, name, money, inventory):
+        self.name = name
+        self.money = money
+        self.inventory = inventory
+
+    def buy(self, item):
+        self.inventory.append(item)
+        print(self.inventory)
+
+Rock = Hero("Rock", 2000, ["sword"])
+Rock.buy({"tite": "wood armor", "defense": +5})
+print(Rock.__dict__) """
+
+
+class pet:
+    def __init__(self, name, happiness, play):
+        self.name = name
+        self.happiness = happiness
+        self.play = play
+
+    def play_time(self, happiness, play):
+        self.happiness.append(play)
+        print(self.happiness)
+
+dog = pet("donkey", 5, ["no play"])
+dog.play_time({"play": "fetch", "happiness": +5})
+print(dog.happiness)
