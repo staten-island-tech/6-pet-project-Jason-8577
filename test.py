43c967249a7358072a0cbceab4b0b09b0440b0de
Jason-8577 <jasono119@nycstudents.net>
2025-11-26 06:25:43 -0800

    Update test.py

--- test.py
+++ test.py
@@ -12,11 +12,6 @@ Rock = Hero("Rock", 2000, ["sword"])
 Rock.buy({"tite": "wood armor", "defense": +5})
 print(Rock.__dict__) """
 
-print("1: Rest")
-print("2: Eat.")
-print("3: play.")
-print("4: show_status.")
-print("5: Exit")
 
 class pet:
     def __init__(self, name, happiness, hunger, energy):
@@ -25,28 +20,36 @@ class pet:
         self.hunger = hunger
         self.energy = energy
         
-    def rest(self, sleep):
-        self.energy += sleep
+    def rest(self):
+        self.energy += 50
+        self.hunger -= 20
 
     def eat(self):
-        self.hunger += 10
-        self.energy += 5
-
-    def play(self, happy):
-        self.__happiness += happy
+        if self.__happiness >= 4:
+            return "Donkey is too sad to eat."
+        self.hunger += 50
+        self.energy += 15
+        
+    def play(self):
+        if self.energy >= 24:
+            return "Donkey has insufficient energy."
+        if self.hunger >= 14:
+            return "Donkey is hungry."
+        self.__happiness += 25
+        self.energy -= 25
+        self.hunger -= 15
 
     def show_status(self):
-        print(f"{self.name}'s happiness is {self.__happiness}, and {self.name}'s energy is {self.energy}.")
+        return f"{self.name}'s happiness is {self.__happiness}"
+
+donkey = pet("donkey", 50, 50, 50)
+
+donkey.play()
+print(donkey.__dict__)
 
 
-class BankAccount:
-    def __init__(self, owner, balance):
-        self.owner = owner
-        self.__balance = balance
 
-    def deposit(self, amount):
-        self.__balance += amount
 
-    def show_balance(self):
-        print(f"{self.owner} has ${self.__balance}")
 
+user_input = input("Input a game you would like to play with donkey.")
+print(f"donkey is playing {user_input}.")
