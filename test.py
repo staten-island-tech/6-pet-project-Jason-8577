0ed02b0efa796545e1460b6d6f10b6b841a1ac6a
Jason-8577 <jasono119@nycstudents.net>
2025-12-02 06:20:03 -0800

    Update test.py

--- test.py
+++ test.py
@@ -19,36 +19,51 @@ class pet:
         self.__happiness = happiness
         self.hunger = hunger
         self.energy = energy
-        
-    def rest(self):
-        self.energy += 50
-        self.hunger -= 20
 
     def eat(self):
-        if self.__happiness >= 4:
-            return f"{self.name} is too sad to eat."
-        self.hunger += 50
-        self.energy += 15
+        self.hunger += 5
+        self.energy += 2
+        self.__happiness += 2
+        print(f"{self.name} is eating {user_input}.")
+        print(f"{self.name}'s happiness, energy, and hunger is now:")
+        print(f"Happiness: {self.__happiness}")
+        print(f"Hunger: {self.hunger}")
+        print(f"Energy: {self.energy}")
         
     def play(self):
-        if self.energy >= 24:
-            return f"{self.name} has insufficient energy."
-        if self.hunger >= 14:
-            return f"{self.name} is hungry."
-        self.__happiness += 25
-        self.energy -= 25
-        self.hunger -= 15
+        self.energy -= 2
+        self.__happiness += 2
+        print(f"{self.name} is playing {user_input}, Happiness level is {self.__happiness} and Energy level is {self.energy}.")
 
     def show_status(self):
-        return f"{self.name}'s happiness is {self.__happiness}"
+        print(f"{self.name}'s status:")
+        print(f"Happiness level: {self.__happiness}.")
+        print(f"Energy level: {self.energy}.")
+        print(f"Hunger level: {self.hunger}.")
         
-donkey = pet("donkey", 100, 100, 100)
+donkey = pet("donkey", 10, 10, 10)
+
 while True:
 
-    print("1: Rest.")
-    print("2: eat.")
-    print("3: play.")
-    print("4: Show_Status.")
+    print("1: eat.")
+    print("2: play.")
+    print("3: Show_Status.")
+    print("4: quit/stop the game.")
 
-    user_input = input("Choose an option.")
-    if 
+    inputs = int(input("input a number."))
+
+    if inputs == 1:
+        user_input = input("What would you like to feed donkey?")
+        donkey.eat()
+
+    elif inputs == 2:
+        user_input = input("What would you like to play with donkey?")
+        donkey.play()
+
+    elif inputs == 3:
+        donkey.show_status()
+    
+    elif inputs == 4:
+        print("Final happiness, energy, and hunger level:")
+        donkey.show_status()
+        break
